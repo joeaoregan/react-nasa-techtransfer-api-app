a56c5bc0f3e73642ef281795b0758f6f4a0e6a4a
joeaoregan <joe.oregan@gmail.com>
2025-06-20 14:29:28 +0100

    image title, regex to strip html

--- src/components/Airplane.jsx
+++ src/components/Airplane.jsx
@@ -1,9 +1,15 @@
 function Airplane({ data }) {
+    const regex = /(<([^>]+)>)/gi;
+    const strippedTitle = data[2].replace(regex, "");
+
     return (
         <div className="w-[400px] p-[10px]">
-            <img src={data[10]} className="hover:scale-105" />
+            <img src={data[10]}
+                className="rounded-3xl hover:scale-105 hover:rounded-none"
+                title={strippedTitle}
+            />
             {/* <h3>{data[2]}</h3> */}
-            <h2 dangerouslySetInnerHTML={{__html:data[2]}} className="text-justify"></h2>
+            <h2 dangerouslySetInnerHTML={{ __html: data[2] }} className="text-justify"></h2>
         </div>
     )
 }
